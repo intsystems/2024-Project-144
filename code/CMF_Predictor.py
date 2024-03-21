c42c1769212ca7164384fabd42012b651bdf380a
koltastic <krekhov.na@phystech.edu>
2024-03-21 15:13:04 +0300

    change interations 2

--- code/CMF_Predictor.py
+++ code/CMF_Predictor.py
@@ -3,25 +3,28 @@ import pandas as pd
 from cmfrec import CMF_implicit
 
 
-
 class CMFRecommender:
-    def __init__(self, num_of_factors=40):
+    def __init__(self, capacity=500, num_of_factors=40):
         self.model = CMF_implicit(k=num_of_factors)
         self.ratings = None
         self.trained = False
+        self.capacity = capacity
 
     def fit(self, ratings, user_info, item_info):
         self.trained = True
-        self.ratings = ratings
+        self.ratings = pd.DataFrame(ratings, columns=['UserId', 'ItemId', 'Rating'])
         self.user_info = user_info
         self.item_info = item_info
-        self.model.fit(X=ratings, U=user_info, I=item_info)
+        self.model.fit(X=self.ratings, U=user_info, I=item_info)
 
     def get_interacted_items(self, user_id):
         return self.ratings.loc[self.ratings.UserId == user_id]['ItemId'].unique()
 
     def get_unqiue_item_count(self):
         return len(self.ratings["ItemId"].unique())
+
+    def get_known_users_info(self):
+        return self.user_info
 
     def recommend_items_new(self, user_id, I, topn):
         recommended_items = self.model.topN_new(user=user_id, n=topn, output_score=True)[:2]
@@ -35,21 +38,30 @@ class CMFRecommender:
     def get_max_index(self):
         return self.ratings["UserId"].max(), self.ratings["ItemId"].max()
 
-    def recommend_items(self, user_id=None, topn=10, exclude_rated=True):
+    def recommend_items(self, user_id, items_to_recommend, topn=10, exclude_rated=True):
         items_to_ignore = []
         if exclude_rated:
             items_to_ignore.extend(self.get_interacted_items(user_id))
-        n = min(topn, self.get_unqiue_item_count() - len(items_to_ignore))
 
-        recommended_items = self.model.topN(user=user_id, n=n, exclude=items_to_ignore, output_score=True)[:2]
+
+        items_to_recommend= np.setdiff1d(items_to_recommend, items_to_ignore)
+        n = min(topn, len(items_to_recommend))
+        recommended_items = self.model.topN(user=user_id, n=n, include=items_to_recommend, output_score=True)
 
         return pd.DataFrame({"ItemId": recommended_items[0], "Rating": recommended_items[1]})
 
     def retrain(self, new_ratings, new_users, new_items):
         number_of_new_ratings = len(new_ratings)
-        new_ratings = pd.DataFrame(new_ratings, columns =['UserId', 'ItemId', 'Rating'])
-        self.ratings = pd.concat([self.ratings.loc[number_of_new_ratings:], new_ratings], ignore_index=True)
-        self.user_info = pd.concat([self.user_info, new_users])
-        self.item_info = pd.concat([self.item_info, new_items])
+        new_ratings = pd.DataFrame(new_ratings, columns=['UserId', 'ItemId', 'Rating'])
+        self.ratings = pd.concat([self.ratings, new_ratings], ignore_index=True)
+        self.truncate_to_capacity()
+        if len(new_users) != 0:
+            self.user_info = pd.concat([self.user_info, new_users])
+        if len(new_items) != 0:
+            self.item_info = pd.concat([self.item_info, new_items])
 
         self.fit(ratings=self.ratings, user_info=self.user_info, item_info=self.item_info)
+
+    def truncate_to_capacity(self):
+        if self.ratings.shape[0] > self.capacity:
+            self.ratings = self.ratings[self.ratings.shape[0] - self.capacity:]
